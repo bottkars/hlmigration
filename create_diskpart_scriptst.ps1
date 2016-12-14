26f8bd01be0a1650f8b7850fc4286ea1abea10ac
Karsten Bott <Karsten.Bott@bottnet.de>
2016-12-14 14:24:25 +0100

    first run

---
+++ create_diskpart_scriptst.ps1
@@ -0,0 +1,50 @@
+﻿
+$Serverprefix1 = "-KAL2-CS-0802"
+$Serverprefix2 = "-FA27-CS-0802"
+
+$counter = 1 
+foreach ($DBcount in (1..10))
+    {
+    $logoffset = 42
+    $dboffset = 62
+    $DBNUM = "{0:D2}" -f $DBcount
+    $LOG = "LOG$DBNUM$Serverprefix1"
+    $DB = "DB$DBNUM$Serverprefix1"
+    $logdisk = $logoffset + $counter
+    $dbdisk = $dboffset + $counter
+    Write-Host "$LOG $logdisk "
+    Write-Host "$DB $dbdisk"
+    ##### create dispart file
+    $diskpart_content_db = @()
+    $diskpart_content_db += "sel dis $dbdisk"
+    $diskpart_content_db += "online dis"
+    $diskpart_content_db += "attrib dis clear readonly"
+    $diskpart_content_db += "cre par pri"
+    $diskpart_content_db += "assign mount=E:\mnt\dbs\$DB"
+    $diskpart_content_db += "FORMA FS=NTFS LABEL=$DB(VNX) UNIT=64k QUICK"
+    $diskpart_content_db | Set-Content -Path ".\dbdisk$dbdisk.txt"
+
+    ##### create dispart file
+    $diskpart_content_log = @()
+    $diskpart_content_log += "sel dis $logdisk"
+    $diskpart_content_log += "online dis"
+    $diskpart_content_log += "attrib dis clear readonly"
+    $diskpart_content_log += "cre par pri"
+    $diskpart_content_log += "assign mount=E:\mntlogs\$LOG"
+    $diskpart_content_log += "FORMA FS=NTFS LABEL=$LOG(VNX) UNIT=64k QUICK"
+    $diskpart_content_log | Set-Content -Path ".\logdisk$logdisk.txt"
+
+    $counter ++
+    }
+
+
+
+    
+
+$diskpart_content = @()
+$content += "sel dis $dis"
+$content += "online dis"
+$content += "attrib dis clear readonly"
+$content += "cre par pri"
+$content += "assign mount=c:\test"
+$content += "FORMAT FS=NTFS LABEL=LABEL UNIT=64k QUICK"
---
+++ scratch1.ps1
@@ -0,0 +1,27 @@
+﻿Get-MailboxDatabaseCopyStatus -Local | where {$_.ActiveCopy -match $false}
+
+
+
+###path analysis
+
+### get mailbox database path and extract partition info
+
+
+$Domain = $env:USERDOMAIN
+$PlainPassword = "Password123!"
+$DomainUser = "$Domain\Administrator"
+$SecurePassword = $PlainPassword | ConvertTo-SecureString -AsPlainText -Force
+$Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $DomainUser, $SecurePassword
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$env:COMPUTERNAME/PowerShell/ -Authentication Kerberos -Credential $Credential
+Import-PSSession $Session -AllowClobber
+$myfiles = @()
+$myfiles = Get-MailboxDatabase -Identity DB2 | Get-MailboxDatabaseCopyStatus | Select-Object DatabaseName -unique | foreach  {(Get-MailboxDatabase $_.databasename).EDBFilePath}
+
+foreach ($myfile in $myfiles)
+    {
+    [System.IO.FileInfo]$myfile = $myfile
+    $myfile
+    
+    Get-Partition | where {$_.AccessPaths -match  $myfile.Directory.Parent}
+    }
+    
